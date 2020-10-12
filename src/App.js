a894dbf8c97a06a9edf42aa52b2ee781e97fa252
groslapinbleu <antoine.jouannais@gmail.com>
2020-10-12 15:19:41 +0200

    app qui marche

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import { sampleText } from './sampleText'
+import marked from 'marked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  componentDidMount() {
+    const text = localStorage.getItem('text')
+    if (text) {
+      this.setState({text})
+    }
+  }
+
+  componentDidUpdate() {
+    const text = this.state.text
+    localStorage.setItem('text', text)
+  }
+
+  renderText = (text) => {
+    const __html = marked(text, { sanitize: true })
+    return {__html}
+  }
+
+  handleChange = (event) => {
+    //console.log(event.target.value)
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea
+              className="form-control"
+              rows="35"
+              value={this.state.text}
+              onChange={this.handleChange}>
+
+            </textarea>
+          </div>
+          <div className="col-sm-6">
+            <div 
+            dangerouslySetInnerHTML={this.renderText(this.state.text)}>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
